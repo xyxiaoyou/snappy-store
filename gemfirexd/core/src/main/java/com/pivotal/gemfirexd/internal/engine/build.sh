4b2181ace4b7cb8daf6423185ae32f0d69f4bb50
hemanth meka <hmeka@snappydata.io>
2018-10-12 18:31:30 +0530

    changes related to review comments and enhances

--- gemfirexd/core/src/main/java/com/pivotal/gemfirexd/internal/engine/build.sh
+++ gemfirexd/core/src/main/java/com/pivotal/gemfirexd/internal/engine/build.sh
@@ -1,39 +1,67 @@
 #!/bin/sh
 
-rm -f *.so
+##################
+# helper functions
+##################
+is_file_generated(){
+  file_path=$1
+  if [ -e "$file_path" ];then
+    file_name=`basename $file_path`
+    echo "SUCCESS: file $file_name generated"
+  else
+    echo "ERROR: file_path $file_path not generated"
+    exit 1
+  fi
+}
+
+rm_if_exists(){
+  file_path=$1
+  if [ -e "$file_path" ];then
+    echo "WARNING: deleting $file_path"
+    rm $file_path
+  fi
+}
 
 CURR_DIR=`pwd`
-cd $CURR_DIR
-
 script_real_path=`realpath $0`
+# script home is assumed to be
+# <SNAPPY_HOME>/store/gemfirexd/core/src/main/java/com/pivotal/gemfirexd/internal/engine/
 script_home=`dirname $script_real_path`
+snappy_store_home=$script_home/../../../../../../../../../../
+gemfire_core_java=$snappy_store_home/gemfire-core/src/main/java/
+gemfirexd_core_java=$script_home/../../../../../
+# modify below variables as per requirement
+gemfire_version="\"7.5.Beta\""
+gemfire_buildid="\"1234\""
+gemfire_builddate="\"2018-10-12\""
+gemfire_buildos="\"linux\""
+
+rm_if_exists $script_home/libgemfirexd.so
+rm_if_exists $script_home/libgemfirexd64.so
+
 
 # generate and fetch ./com_gemstone_gemfire_internal_NanoTimer.h
-cd $script_home/../../../../../../../../../../gemfire-core/src/main/java/
-javah  com.gemstone.gemfire.internal.NanoTimer
-mv ./com_gemstone_gemfire_internal_NanoTimer.h $script_home/
-cd -
-if [ -e $script_home/com_gemstone_gemfire_internal_NanoTimer.h ];then
-  echo "SUCCESS: com_gemstone_gemfire_internal_NanoTimer.h generated "
-fi
+cd $gemfire_core_java
+javah -d $script_home/ com.gemstone.gemfire.internal.NanoTimer
+is_file_generated $script_home/com_gemstone_gemfire_internal_NanoTimer.h
+
 
 # generate and fetch com_pivotal_gemfirexd_internal_GemFireXDVersion.h
-cd $script_home/../../../../../
-ppp=`pwd`
-echo "currently at $ppp"
-javah com.pivotal.gemfirexd.internal.GemFireXDVersion
-mv ./com_pivotal_gemfirexd_internal_GemFireXDVersion.h $script_home/
-cd -
-if [ -e $script_home/com_pivotal_gemfirexd_internal_GemFireXDVersion.h ];then
-  echo "SUCCESS: com_pivotal_gemfirexd_internal_GemFireXDVersion.h generated "
-fi
+cd $gemfirexd_core_java
+javah -d $script_home/ com.pivotal.gemfirexd.internal.GemFireXDVersion
+is_file_generated $script_home/com_pivotal_gemfirexd_internal_GemFireXDVersion.h
 
+# generate so files
 cd $script_home
 
-gcc -m32 -O3 -fPIC -DPIC -D_REENTRANT -I $JAVA_HOME/include -I $JAVA_HOME/include/linux -I $script_home/ -shared utils.c jvmkill.c -o libgemfirexd.so
-gcc -m64 -O3 -fPIC -DPIC -D_REENTRANT -I $JAVA_HOME/include -I $JAVA_HOME/include/linux -I $script_home/ -shared utils.c jvmkill.c -o libgemfirexd64.so
+
+gcc -DGEMFIRE_VERSION=$gemfire_version -DGEMFIRE_BUILDID=$gemfire_buildid -DGEMFIRE_BUILDDATE=$gemfire_builddate -DGEMFIRE_BUILDOS=$gemfire_buildos -m32 -O3 -fPIC -DPIC -D_REENTRANT -I $JAVA_HOME/include -I $JAVA_HOME/include/linux -I $script_home/ -shared utils.c jvmkill.c -o libgemfirexd.so
+is_file_generated $script_home/libgemfirexd.so
+
+gcc -DGEMFIRE_VERSION=$gemfire_version -DGEMFIRE_BUILDID=$gemfire_buildid -DGEMFIRE_BUILDDATE=$gemfire_builddate -DGEMFIRE_BUILDOS=$gemfire_buildos  -m64 -O3 -fPIC -DPIC -D_REENTRANT -I $JAVA_HOME/include -I $JAVA_HOME/include/linux -I $script_home/ -shared utils.c jvmkill.c -o libgemfirexd64.so
+is_file_generated $script_home/libgemfirexd64.so
 
 chmod -x *.so
 
-
+# go back to initial working dir
 cd $CURR_DIR
