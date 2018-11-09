875c77182b557d4f1e608327d1f3b93a448353bd
snappy-sachin <skapse@snappydata.io>
2018-11-09 17:51:55 +0530

    SNAP-2602 : On snappy UI, add column named "Overflown Size"/ "Disk Size" in Tables. (#440)
    * Changes for SNAP-2602:
     - SnappyRegionStats is updated for holding tables size spillover to disk i.e. tables overflown size.
     - SnappyRegionStatsCollectorFunction is updated to set tables size spillover to disk.
     - and relevant code changes.

--- gemfirexd/core/src/main/java/com/pivotal/gemfirexd/internal/engine/ui/SnappyRegionStats.java
+++ gemfirexd/core/src/main/java/com/pivotal/gemfirexd/internal/engine/ui/SnappyRegionStats.java
@@ -31,6 +31,7 @@ public class SnappyRegionStats implements VersionedDataSerializable {
   private String tableName;
   private long rowCount = 0;
   private long sizeInMemory = 0;
+  private long sizeSpillToDisk = 0;
   private long totalSize = 0;
   private boolean isReplicatedTable = false;
   private int bucketCount;
@@ -48,14 +49,11 @@ public class SnappyRegionStats implements VersionedDataSerializable {
     this.tableName = tableName;
     this.totalSize = totalSize;
     this.sizeInMemory = sizeInMemory;
+    this.sizeSpillToDisk = totalSize - sizeInMemory;
     this.rowCount = rowCount;
     this.isColumnTable = isColumnTable;
     this.isReplicatedTable = isReplicatedTable;
     this.bucketCount = bucketCount;
-  }
-
-  public void setTotalSize(long totalSize) {
-    this.totalSize = totalSize;
   }
 
   public String getTableName() {
@@ -99,8 +97,20 @@ public class SnappyRegionStats implements VersionedDataSerializable {
     this.sizeInMemory = sizeInMemory;
   }
 
+  public long getSizeSpillToDisk() {
+    return sizeSpillToDisk;
+  }
+
+  public void setSizeSpillToDisk(long sizeSpillToDisk) {
+    this.sizeSpillToDisk = sizeSpillToDisk;
+  }
+
   public long getTotalSize() {
     return this.totalSize;
+  }
+
+  public void setTotalSize(long totalSize) {
+    this.totalSize = totalSize;
   }
 
   public int getBucketCount() {
@@ -122,6 +132,7 @@ public class SnappyRegionStats implements VersionedDataSerializable {
     }
 
     combinedStats.setSizeInMemory(stats.sizeInMemory + this.sizeInMemory);
+    combinedStats.setSizeSpillToDisk(stats.sizeSpillToDisk + this.sizeSpillToDisk);
     combinedStats.setTotalSize(stats.totalSize + this.totalSize);
     combinedStats.setColumnTable(this.isColumnTable || stats.isColumnTable);
     combinedStats.setReplicatedTable(this.isReplicatedTable());
@@ -140,6 +151,7 @@ public class SnappyRegionStats implements VersionedDataSerializable {
     DataSerializer.writeString(tableName, out);
     out.writeLong(totalSize);
     out.writeLong(sizeInMemory);
+    out.writeLong(sizeSpillToDisk);
     out.writeLong(rowCount);
     out.writeBoolean(isColumnTable);
     out.writeBoolean(isReplicatedTable);
@@ -155,6 +167,7 @@ public class SnappyRegionStats implements VersionedDataSerializable {
     this.tableName = DataSerializer.readString(in);
     this.totalSize = in.readLong();
     this.sizeInMemory = in.readLong();
+    this.sizeSpillToDisk = in.readLong();
     this.rowCount = in.readLong();
     this.isColumnTable = in.readBoolean();
     this.isReplicatedTable = in.readBoolean();
@@ -169,8 +182,8 @@ public class SnappyRegionStats implements VersionedDataSerializable {
   @Override
   public String toString() {
     return "RegionStats for " + tableName + ": totalSize=" + totalSize +
-        " sizeInMemory=" + sizeInMemory + " rowCount=" + rowCount +
-        " isColumnTable=" + isColumnTable + " isReplicatedTable=" +
+        " sizeInMemory=" + sizeInMemory + " sizeSpillToDisk=" + sizeSpillToDisk +
+        " rowCount=" + rowCount + " isColumnTable=" + isColumnTable + " isReplicatedTable=" +
         isReplicatedTable + " bucketCount=" + bucketCount;
   }
 }
