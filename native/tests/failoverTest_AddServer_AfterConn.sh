bf3e1ce3544fcf94f465213b89f73c8012d773db
Piyush Bisen <pbisen@snappydata.io>
2019-07-30 13:24:30 +0530

    Changes the shell script and cpp files name

--- native/tests/failoverTest_AddServer_AfterConn.sh
+++ native/tests/failoverTest_AddServer_AfterConn.sh
@@ -25,14 +25,14 @@ headerLoc=${_snappyNativeDir}/build-artifacts/lin/snappyclient/include
 boostHeadeLoc=${distDir}/boost-${BOOST_VERSION}/include
 thriftHeaderLoc=${distDir}/thrift-${THRIFT_VERSION}/include
 
-g++ -std=c++11 -I${headerLoc} -I${boostHeadeLoc} -I${thriftHeaderLoc} -O0 -g3 -Wall -c ${_testDir}/failoverTest_None.cpp -o ${_testDir}/failoverTest_None.o
+g++ -std=c++11 -I${headerLoc} -I${boostHeadeLoc} -I${thriftHeaderLoc} -O0 -g3 -Wall -c ${_testDir}/failoverTest_AddServer_AfterConn.cpp -o ${_testDir}/failoverTest_AddServer_AfterConn.o
 
-chmod 777 ${_testDir}/failoverTest_None.o
+chmod 777 ${_testDir}/failoverTest_AddServer_AfterConn.o
 
-g++ -L${boostLibPath} -L${thrftLibPath} -L${snappClientLibPath} -o ${_testDir}/failoverTest_None  ${_testDir}/failoverTest_None.o  -lsnappyclient -lcrypto -lodbc -lpthread -lssl -lboost_chrono -lboost_date_time -lboost_filesystem -lboost_log -lboost_log_setup -lboost_system -lboost_thread -lthrift -lpthread -lrt -lgmp 
+g++ -L${boostLibPath} -L${thrftLibPath} -L${snappClientLibPath} -o ${_testDir}/failoverTest_AddServer_AfterConn  ${_testDir}/failoverTest_AddServer_AfterConn.o  -lsnappyclient -lcrypto -lodbc -lpthread -lssl -lboost_chrono -lboost_date_time -lboost_filesystem -lboost_log -lboost_log_setup -lboost_system -lboost_thread -lthrift -lpthread -lrt -lgmp 
 
-chmod 777 ${_testDir}/failoverTest_None
+chmod 777 ${_testDir}/failoverTest_AddServer_AfterConn
 cd ${_testDir}
-./failoverTest_None $SNAPPY_HOME_DIR
+./failoverTest_AddServer_AfterConn $SNAPPY_HOME_DIR
 
-rm failoverTest_None failoverTest_None.o
+rm failoverTest_AddServer_AfterConn failoverTest_AddServer_AfterConn.o
